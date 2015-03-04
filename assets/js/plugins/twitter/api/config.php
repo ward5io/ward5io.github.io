0440a5516191fc3b965d4e3c90c1e0e71fbbd386
nwichert <nick.wichert@icloud.com>
2015-03-04 15:12:55 -0600

    initial commit
    moving repos under proper accounts

---
+++ assets/js/plugins/twitter/api/config.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', '8AvFCU6d7d79MDSf1WKJ4dmTQ');
+    define('CONSUMER_SECRET', 'oOzMQv3gOreLXuvlAciyVvMtBNcbzGeAzD1ssS1By0tYOgfqlA');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '2842307849-Qgchq9OiLthWzOAW6tHcrwVk05zSKGjgeF6amtB');
+    define('ACCESS_SECRET', 'I0HoaOIZRGv58GHUr36DONyt1NtJOlt5MNh6wB8PAlfuj');
+	
+	// Cache Settings
+	define('CACHE_ENABLED', false);
+	define('CACHE_LIFETIME', 3600); // in seconds
+	define('HASH_SALT', md5(dirname(__FILE__)));
